e727a2de4175a6e50840bedefb4912257926c5b0
obatres <batres.93@gmail.com>
2020-03-02 20:48:08 -0600

    agregado el While y Do While

---
+++ ProyectoCupJlexWindows-master/src/arbol/DoWhile/DoWhile.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arbol.DoWhile;
+
+import arbol.Expresion;
+import arbol.Instruccion;
+import arbol.Nodo;
+import arbol.TablaDeSimbolos;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author obatres_
+ */
+public class DoWhile extends Instruccion{
+
+    private Expresion exp; 
+    private LinkedList<Nodo> ListaDeInstrucciones;
+
+    public DoWhile(Expresion exp, LinkedList<Nodo> ListaDeInstrucciones) {
+        this.exp = exp;
+        this.ListaDeInstrucciones = ListaDeInstrucciones;
+    }
+    
+    
+    @Override
+    public Object ejecutar(TablaDeSimbolos ts) {
+        do {            
+            TablaDeSimbolos tablalocal = new TablaDeSimbolos();
+            tablalocal.addAll(ts);
+            for (Nodo en : ListaDeInstrucciones) {
+                if(en instanceof Instruccion){
+                    ((Instruccion) en).ejecutar(ts);
+                }  
+            }             
+        } while ((Boolean)exp.ejecutar(ts));
+        return null;
+    }
+    
+}
