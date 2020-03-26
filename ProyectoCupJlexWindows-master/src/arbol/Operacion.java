0d9da26ec29510610704edac642b3bd28fd7c142
obatres <batres.93@gmail.com>
2020-03-25 23:48:22 -0600

    CORREGIDA LA MULTIPLICACION

--- ProyectoCupJlexWindows-master/src/arbol/Operacion.java
+++ ProyectoCupJlexWindows-master/src/arbol/Operacion.java
@@ -237,11 +237,35 @@ public class Operacion extends Expresion{
         }else if(tipo_operacion==Tipo_operacion.MULTIPLICACION){
             if(opderadorIzq.GetTipo(ts).isInt()){
                 if(operadorDer.GetTipo(ts).isInt()||operadorDer.GetTipo(ts).isDouble()){
-                    return Double.parseDouble(opderadorIzq.ejecutar(ts).toString())*Double.parseDouble(operadorDer.ejecutar(ts).toString());
+                    Double val1;
+                    Double val2;
+                    if(opderadorIzq.ejecutar(ts) instanceof ArrayList){
+                        val1=(Double) ((ArrayList)opderadorIzq.ejecutar(ts)).get(0);
+                    }else{
+                        val1= Double.parseDouble(opderadorIzq.ejecutar(ts).toString());                     
+                    }
+                    if(operadorDer.ejecutar(ts) instanceof ArrayList){
+                        val2=(Double) ((ArrayList)operadorDer.ejecutar(ts)).get(0);
+                    }else{
+                        val2= Double.parseDouble(operadorDer.ejecutar(ts).toString());                    
+                    }                    
+                    return val1*val2;
                 }     
             }else if(opderadorIzq.GetTipo(ts).isDouble()){
                 if(operadorDer.GetTipo(ts).isInt()||operadorDer.GetTipo(ts).isDouble()){
-                    return Double.parseDouble(opderadorIzq.ejecutar(ts).toString())*Double.parseDouble(operadorDer.ejecutar(ts).toString());
+                    Double val1;
+                    Double val2;
+                    if(opderadorIzq.ejecutar(ts) instanceof ArrayList){
+                        val1=(Double) ((ArrayList)opderadorIzq.ejecutar(ts)).get(0);
+                    }else{
+                        val1= Double.parseDouble(opderadorIzq.ejecutar(ts).toString());                     
+                    }
+                    if(operadorDer.ejecutar(ts) instanceof ArrayList){
+                        val2=(Double) ((ArrayList)operadorDer.ejecutar(ts)).get(0);
+                    }else{
+                        val2= Double.parseDouble(operadorDer.ejecutar(ts).toString());                    
+                    }                    
+                    return val1* val2;
                 }
             }else{
                 //TODO reportar error de tipo
