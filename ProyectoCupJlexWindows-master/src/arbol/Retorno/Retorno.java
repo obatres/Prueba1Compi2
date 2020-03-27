548997f4621518201320e1e2d2d51eca7d9fbae3
obatres <batres.93@gmail.com>
2020-03-27 16:17:26 -0600

    Return agregado a las instrucciones

---
+++ ProyectoCupJlexWindows-master/src/arbol/Retorno/Retorno.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arbol.Retorno;
+
+import arbol.Expresion;
+import arbol.TablaDeSimbolos;
+import arbol.Tipo;
+
+/**
+ *
+ * @author obatres_
+ */
+public class Retorno extends Expresion{
+    
+    private Expresion exp;
+
+    public Retorno(Expresion exp) {
+        this.exp = exp;
+    }
+    
+    
+    
+    @Override
+    public Object ejecutar(TablaDeSimbolos ts) {
+        return exp.ejecutar(ts);
+    }
+
+    @Override
+    public Tipo GetTipo(TablaDeSimbolos ts) {
+        return exp.GetTipo(ts);
+    }
+
+    @Override
+    public int Dibujar(StringBuilder builder, String parent, int cont) {
+        return cont;
+    }
+    
+}
